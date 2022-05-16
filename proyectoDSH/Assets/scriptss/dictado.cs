b1b6ffb38218afe5e6f455b94d653b62168202da
JAcode9 <72512757+JAcode9@users.noreply.github.com>
2022-05-16 23:23:50 +0200

    Add files via upload

--- proyectoDSH/Assets/scriptss/dictado.cs
+++ proyectoDSH/Assets/scriptss/dictado.cs
@@ -8,9 +8,21 @@ using System.Text;
 using System;
 using UnityEngine.UI;
 using System.Threading;
+using TMPro;
 
 public class dictado : MonoBehaviour
 {
+	[SerializeField] int min, seg;
+	[SerializeField] TextMeshProUGUI tiempo;
+	private float restante;
+	private bool enMarcha;
+
+	private void Awake() {
+		restante = (min * 60) + seg;
+		enMarcha = true;
+	}
+
+
     private DictationRecognizer dictationRecognizer;
     string que;
 
@@ -72,6 +84,15 @@ public class dictado : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		if(enMarcha){
+        restante -= Time.deltaTime;
+		if(restante < 1){
+			enMarcha = false;
+			Debug.Log("Sin tiempo wey");
+		}
+		int tempMin = Mathf.FloorToInt(restante / 60);
+		int tempSeg = Mathf.FloorToInt(restante % 60);
+		tiempo.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
+		}
     }
 }
