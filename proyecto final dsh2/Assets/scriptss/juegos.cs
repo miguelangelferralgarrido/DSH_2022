dd80d1dd37cbb573e745defa344cf2eb35d06266
miguelangelferralgarrido <92022764+miguelangelferralgarrido@users.noreply.github.com>
2022-05-17 19:08:57 +0200

    Añadiendo japon y china a las cosas tipicas

--- proyecto final dsh2/Assets/scriptss/juegos.cs
+++ proyecto final dsh2/Assets/scriptss/juegos.cs
@@ -9,14 +9,27 @@ using UnityEngine.SceneManagement;
 
 public class juegos : MonoBehaviour
 {
+    public ValorPais[] paises;
+    private ValorPais pais;
+    
+    private int  numPaisActual=-1;
+    private GameObject actual;
+    private Vector3 v=new Vector3((float)-0.2571168,(float)0.06874365,(float)0.3922196);
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
     // Start is called before the first frame update
     void Start()
     {
-
+       //Vector3 (0,0.06874365,0.1635039);
+        //transform.position.y=0.06874365;
+        //transform.position.z=0.1635039;
+        //transform.position= v;
+        nextPais();
+        
         actions.Add("Siguiente",siguiente);
         actions.Add("Menu",volver);
+        actions.Add("Japon",japon);
+        actions.Add("China",china);
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
 
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
@@ -35,8 +48,28 @@ public class juegos : MonoBehaviour
     private void siguiente(){
         SceneManager.LoadScene("10");
     }
+    private void japon(){
+       
+        if(actual.CompareTag("japon")){
+            Debug.Log("cagada con las actions");
+           nextPais();
+        }
+    }
+     private void china(){
+       
+        if(actual.CompareTag("china")){
+            Debug.Log("cagada con las actions");
+           nextPais();
+        }
+    }
 
-
+    void nextPais(){
+         numPaisActual++;
+         Destroy(actual);
+        pais=paises[ numPaisActual];
+        actual=Instantiate(pais.pais,pais.pais.transform.position,Quaternion.identity);
+        actual.transform.rotation=pais.pais.transform.rotation;
+    }
     // Update is called once per frame
     void Update()
     {
